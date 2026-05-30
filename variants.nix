ee4ab86a47851100ba754c54277ff843ab4877ab
wliao <lwlw1988@gmail.com>
2026-05-31 05:32:02 +0800

    fix: correct v34.4.0 release asset hash for iosevka-custom-bin

--- variants.nix
+++ variants.nix
@@ -1,5 +1,5 @@
 # Release asset hashes for iosevka-custom-bin.
 # Update this file whenever a new GitHub release asset is published.
 {
-  Custom = "sha256-0RMY5PO2FDR1kQ9KvZXRO7NzPQmDX/X9aOMkXZtkOho=";
+  Custom = "sha256-oDgqFOJhbylEvbLlAJL/yVWpu8kePuFPLtJKLm2AdRo=";
 }
